f318370ba25012824760cf5db81e7f4fbbf7f930
David <dav4thevid@gmail.com>
2020-06-17 23:41:25 +0100

    enhan:added card

--- src/Components/homepage/Homepage.js
+++ src/Components/homepage/Homepage.js
@@ -4,13 +4,13 @@ import WorldCovid19Stats from '../homepage/partials/WorldCovid19Stats'
 
 export default function Homepage() {
   return (
-      <div class = "container-fluid">
-          <div class = "row">
-            <div class = "col-12 col-sm-12 col-md-4">
+      <div className = "container-fluid">
+          <div className = "row">
+            <div className = "col-12 col-sm-12 col-md-4">
               <QuickFacts></QuickFacts>
             </div>
 
-            <div class = "col-12 col-sm-12 col-md-8">
+            <div className = "col-12 col-sm-12 col-md-8">
               {/* <WorldCovid19Stats></WorldCovid19Stats> */}
             </div>
           </div>
