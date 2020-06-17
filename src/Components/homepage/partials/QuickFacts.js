5c4ac1970cd5412cb25ae5b606c96a3ff1e3a8ea
David <dav4thevid@gmail.com>
2020-06-17 23:39:33 +0100

    enhan:changed cards and data-table

--- src/Components/homepage/partials/QuickFacts.js
+++ src/Components/homepage/partials/QuickFacts.js
@@ -1,54 +1,41 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
+import axios from 'axios';
 
-const useStyles = makeStyles({
-  root: {
-    minWidth: 275,
-  },
-  bullet: {
-    display: 'inline-block',
-    margin: '0 2px',
-    transform: 'scale(0.8)',
-  },
-  title: {
-    fontSize: 14,
-  },
-  pos: {
-        marginTop:10,
-        marginBottom: 12,
-  },
-});
+class QuickFact extends React.Component {
+  constructor(props){
+    super(props)
 
-export default function QuickFacts() {
-  const classes = useStyles();
-  const bull = <span className={classes.bullet}>•</span>;
+    this.state = {
+      timeline: {}
+    }
+  }
 
-  return (
-    <Card className={classes.root}>
-      <CardContent>
-        <Typography className={classes.title} color="textSecondary" gutterBottom>
-          Word of the Day
-        </Typography>
-        <Typography variant="h5" component="h2">
-          be{bull}nev{bull}o{bull}lent
-        </Typography>
-        <Typography className={classes.pos} color="textSecondary">
-          adjective
-        </Typography>
-        <Typography variant="body2" component="p">
-          well meaning and kindly.
-          <br />
-          {'"a benevolent smile"'}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Learn More</Button>
-      </CardActions>
-    </Card>
-  );
+  
+
+  componentDidMount() {
+    
+    const url = 'https://corona-api.com/timeline'
+    axios.get(url).then(res => {
+      const timeline1 = res.data.data[0];
+      this.setState({timeline:timeline1});
+      // console.log(this.state.timeline)
+      console.log(this.state.timeline.active);
+    })
+  }
+
+  render() {
+    return (<div>
+     <card>
+      <h3>{this.state.timeline.active}</h3>
+      <h3>{this.state.timeline.deaths}</h3>
+     </card>
+    
+     </div>
+    )
+  }
+
+
 }
+
+
+export default QuickFact
