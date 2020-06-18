998a55174c0bde8d31335d852424e1baf2db250a
David <dav4thevid@gmail.com>
2020-06-18 01:34:38 +0100

    :enhan:made minor changes

--- src/Components/homepage/partials/QuickFacts.js
+++ src/Components/homepage/partials/QuickFacts.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import axios from 'axios';
 
+const DATE_OPTIONS = { weekday: 'short', year: 'numeric', month: 'short', day: 'numeric' };
+
 class QuickFact extends React.Component {
   constructor(props){
     super(props)
@@ -13,23 +15,41 @@ class QuickFact extends React.Component {
   
 
   componentDidMount() {
-    
     const url = 'https://corona-api.com/timeline'
     axios.get(url).then(res => {
       const timeline1 = res.data.data[0];
       this.setState({timeline:timeline1});
-      // console.log(this.state.timeline)
       console.log(this.state.timeline.active);
     })
   }
 
   render() {
     return (<div>
-     <card>
-      <h3>{this.state.timeline.active}</h3>
-      <h3>{this.state.timeline.deaths}</h3>
-     </card>
-    
+      <div class = "row ">
+        <div class = "col">
+        <div class="card ">
+      <div class="card-body  ">
+        <h2 class="card-title">QUICK FACTS</h2>
+        <small>{this.state.timeline.updated_at}</small>
+        <h3 class="card-header">{this.state.timeline.confirmed}</h3>
+        <h5 class="card-text">Total-Confirmed</h5>
+        <h3 class="card-header">{this.state.timeline.active}</h3>
+        <h5 class="card-text">Total-Active</h5>
+        <h3 class = "card-header">{this.state.timeline.recovered}</h3>
+        <h5 clas = "card-text">Total Recovered</h5>
+        <h3 class = "card-header">{this.state.timeline.deaths}</h3>
+        <h5 clas = "card-text">Total Deaths</h5>
+        <h3 class = "card-header">{this.state.timeline.new_confirmed}</h3>
+        <h5 clas = "card-text">Newly Confirmed</h5>
+        <h3 class = "card-header">{this.state.timeline.new_recovered}</h3>
+        <h5 clas = "card-text">Newly Recovered</h5>
+        <h3 class = "card-header">{this.state.timeline.new_deaths}</h3>
+        <h5 clas = "card-text">New Deaths</h5> 
+      </div>
+    </div>
+        </div>
+      </div>
+   
      </div>
     )
   }
