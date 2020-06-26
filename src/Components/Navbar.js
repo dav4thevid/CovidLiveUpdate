aeab7a410c84d771aa4e8b8aafe8539db7c5a71c
David <dav4thevid@gmail.com>
2020-06-26 02:43:16 +0100

    :bugfix:fixed navbar bug

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
+import { Navbar, Nav, NavDropdown,  } from 'react-bootstrap';
 
 function Navigation() {
     return (
